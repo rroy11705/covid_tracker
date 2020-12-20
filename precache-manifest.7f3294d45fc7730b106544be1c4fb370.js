af51635220818c61605d0bf9779bff6b3f70fdef
YOUR_USERNAMErroy11705 <rroy11705@gmail.com>
2020-12-20 13:10:19 +0530

    Updates

---
+++ precache-manifest.7f3294d45fc7730b106544be1c4fb370.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ce07c173132e79848b92138d9bc5109",
+    "url": "/covid_tracker/index.html"
+  },
+  {
+    "revision": "20423525a03d5c058d8f",
+    "url": "/covid_tracker/static/css/main.c7596b0c.chunk.css"
+  },
+  {
+    "revision": "04da69941c4eb4ce0b36",
+    "url": "/covid_tracker/static/js/2.146671d5.chunk.js"
+  },
+  {
+    "revision": "5ba854edbe2007b917d7e6843a84f808",
+    "url": "/covid_tracker/static/js/2.146671d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20423525a03d5c058d8f",
+    "url": "/covid_tracker/static/js/main.91c2070a.chunk.js"
+  },
+  {
+    "revision": "faf54d89f29a50d0927b",
+    "url": "/covid_tracker/static/js/runtime-main.32eb6b33.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid_tracker/static/media/image.d7265326.png"
+  }
+]);
